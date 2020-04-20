f63517f2ffdd16d7f65269c33ef90b961c21543b
Bailey Day <bailey.j.day@gmail.com>
2020-04-20 18:43:51 -0400

    Fixed styling issues associated with material UI, added a styled component

--- client/src/components/styledComponents.js
+++ client/src/components/styledComponents.js
@@ -1,18 +1,19 @@
-
-
 // Crystal clear = #DCEDE9
 // Tricorn Black = #2F2F2F
 // Halcyon Green = #9BAAA3
 // Inverness Green = #576238
 // Glitzy Gold = #D7A02B
 
-import styled from 'styled-components'
-import { Button } from "@material-ui/core";
+import styled from "styled-components";
+import { Button, Link } from "@material-ui/core";
 
 export const GreenButton = styled(Button)`
-    background-color: #576238;
-    :hover {
-        background-color: #3b4225;
-    }
-`
+  background-color: #576238;
+  :hover {
+    background-color: #3b4225;
+  }
+`;
 
+export const BlackLink = styled(Link)`
+  color: #2f2f2f;
+`;
