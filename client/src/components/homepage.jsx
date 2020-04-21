9a8fdd9591b392e755cb75c6324c187dbbc9634b
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-21 14:03:45 -0400

    brought in Gordys changes

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import NavBar from "../materialComponents/navbar";
+import React from 'react';
+import NavBar from '../materialComponents/navbar';
 
 function HomePage() {
   return (
