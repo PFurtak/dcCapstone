a5ece7a1ce21d5969af99861c854546f94945ac9
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-21 15:39:37 -0400

    Added in user sign up, brought in json web token to be written to local storage on successful registration, added a proxy setting to make talking to the back end easier from client package.json. Verified new users are being written to the DB

--- client/src/context/auth/authReducer.jsx
+++ client/src/context/auth/authReducer.jsx
@@ -11,5 +11,33 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
+    default:
+      return state;
   }
 };
