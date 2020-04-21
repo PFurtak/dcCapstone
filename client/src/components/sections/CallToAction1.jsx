6582b1d9e04c819770392d22bd894dba43e41df7
Bailey Day <bailey.j.day@gmail.com>
2020-04-21 16:47:37 -0400

    Finished styling of preliminary homepage

--- client/src/components/sections/CallToAction1.jsx
+++ client/src/components/sections/CallToAction1.jsx
@@ -6,8 +6,15 @@ class CallToAction1 extends Component {
   render() {
     let { bg } = this.props;
     return (
-      <div className="section section-cta1" id="cta1"
-      style={{background: `url(${bg || './assets/images/home-bg.jpg'}) center center/cover no-repeat`}}>
+      <div
+        className="section section-cta1"
+        id="cta1"
+        style={{
+          background: `url(${
+            bg || "./assets/images/home-bg-black.png"
+          }) center center/cover no-repeat`,
+        }}
+      >
         <div className="container">
           <Grid
             container
@@ -17,15 +24,14 @@ class CallToAction1 extends Component {
             justify="flex-start"
           >
             <Grid item lg={8} md={8} sm={12} xs={12}>
-              <h2>Dont miss the best management tool of the world</h2>
+              <h2>Join now!</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-                nam, illum et quis officiis beatae.
+                Dont miss out, sign up now to start building your own funds!
               </p>
             </Grid>
             <Grid item lg={4} md={4} sm={12} xs={12} className="text-center">
-              <Button size="large" color="secondary" variant="contained">
-                Start 14 days Trial
+              <Button size="large" href="/signup" variant="contained">
+                Sign Up Now
               </Button>
             </Grid>
           </Grid>
