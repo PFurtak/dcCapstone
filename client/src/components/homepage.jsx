d8ceeb1c4d6ffba1b2e39b9eb78942b864a6174f
George M Bird <gmbirdiv@gmail.com>
2020-04-21 13:11:09 -0400

    updated hp

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import NavBar from "../materialComponents/navbar";
-import InputFund from './InputFund'
 
 function HomePage() {
   return (
     <div>
       <NavBar />
       <h1>HomePage</h1>
-      <InputFund/>
     </div>
   );
 }
