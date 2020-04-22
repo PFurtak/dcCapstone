ac92912b604d335af92bfe82068638c1298a0462
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-22 11:53:13 -0400

    brought in loadUser function to home page so authentication status persists when navigating to home page

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { scrollTo } from '../Utils';
 import Intro2 from './sections/Intro2';
 import TopBar from './sections/TopBar1';
@@ -9,23 +9,28 @@ import CallToAction1 from './sections/CallToAction1';
 import Pricing1 from './sections/Pricing1';
 import Contact1 from './sections/Contact1';
 import Footer1 from './sections/Footer1';
+import AuthContext from '../context/auth/authContext';
 
-class Landing1 extends Component {
-  state = {};
-  componentWillUnmount() {
+const Landing1 = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    //eslint-disable-next-line
+  }, []);
+
+  const componentWillUnmount = () => {
     scrollTo('root');
-  }
-  render() {
-    return (
-      <div className='landing'>
-        <TopBar />
-        <Intro2 />
-        <Services1 />
-        <CallToAction1 />
-        <Footer1 />
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className='landing'>
+      <TopBar />
+      <Intro2 />
+      <Services1 />
+      <CallToAction1 />
+      <Footer1 />
+    </div>
+  );
+};
 
 export default Landing1;
