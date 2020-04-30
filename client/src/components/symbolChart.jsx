327f89ae5ddcdd3210db5f5fc08b3e6a7e0be859
Bailey Day <bailey.j.day@gmail.com>
2020-04-30 15:22:42 -0400

    Fixed symbol chart to work on button click, minor styling changes

--- client/src/components/symbolChart.jsx
+++ client/src/components/symbolChart.jsx
@@ -1,29 +1,45 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { AreaChart, PieChart } from 'react-chartkick';
-import 'chart.js';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import FundContext from '../context/funds/fundContext';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect, useContext } from "react";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { AreaChart, PieChart } from "react-chartkick";
+import "chart.js";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import FundContext from "../context/funds/fundContext";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import { GreenButton } from "./styledComponents";
 
 const useStyles = makeStyles({
+  cssLabel: {
+    "&$cssFocused": {
+      color: "#2F2F2F",
+    },
+  },
+  cssOutlinedInput: {
+    "&$cssFocused $notchedOutline": {
+      borderColor: "#2F2F2F", //focused
+      color: "#2F2F2F",
+    },
+  },
+  notchedOutline: {},
+  cssFocused: {},
+  error: {},
+  disabled: {},
   root: {
     minWidth: 275,
     marginTop: 20,
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     fontSize: 14,
@@ -37,18 +53,15 @@ const SymbolChart = () => {
   const classes = useStyles();
   const [spchartData, setspChartData] = useState([]);
   const [naschartData, setnasChartData] = useState([]);
-  const [diachartData, setdiaChartData] = useState([]);
   const [spchartMin, setspchartMin] = useState(0);
-  const [diachartMin, setdiachartMin] = useState(0);
-  const [naschartMin, setnaschartMin] = useState(0);
-  const fundContext = useContext(FundContext);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [searchArray, setSearchArray] = useState([]);
   const [quote, setQuote] = useState(0);
   const [pickedSecurity, setPickedSecurity] = useState({});
-  const [pickedSymbol, setPickedSymbol] = useState('');
+  const [pickedSymbol, setPickedSymbol] = useState("");
   const [data, setData] = useState([]);
-  const [symbolName, setSymbolName] = useState('');
+  const [symbolName, setSymbolName] = useState("");
+  const [symbolCall, setSymbolCall] = useState("");
 
   const [value, setValue] = React.useState(2);
 
@@ -64,7 +77,16 @@ const SymbolChart = () => {
       setInput(newValue.symbol);
       setPickedSymbol(newValue.symbol);
       setPickedSecurity(newValue.securityName);
+      setspChartData([]);
     } catch {}
+  };
+
+  const getSymbolInformation = () => {
+    console.log("Button Clicked");
+    try {
+      setSymbolCall(pickedSymbol);
+    } catch {}
+    console.log(symbolCall);
   };
 
   useEffect(
@@ -72,7 +94,7 @@ const SymbolChart = () => {
       async function getData() {
         try {
           const res = await fetch(
-            `https://cloud.iexapis.com/stable/stock/${pickedSymbol}/intraday-prices?token=pk_135e66691d174c4291a33989af3f52c9`
+            `https://cloud.iexapis.com/stable/stock/${symbolCall}/intraday-prices?token=pk_135e66691d174c4291a33989af3f52c9`
           ).then((res) => res.json());
 
           let spchartData = res;
@@ -91,7 +113,7 @@ const SymbolChart = () => {
       }
       getData();
     },
-    [pickedSymbol]
+    [symbolCall]
   );
 
   console.log(spchartData);
@@ -118,18 +140,18 @@ const SymbolChart = () => {
   );
 
   return (
-    <Card className={classes.root} id='mainChartCard'>
+    <Card className={classes.root} id="mainChartCard">
       <CardContent>
-        <h1 className='title'>{symbolName}</h1>
+        <h1 className="title">{symbolName}</h1>
         <AreaChart
           data={spchartData}
           min={spchartMin}
           points={false}
-          xtitle='Time of day'
-          ytitle='Average Price'
-          colors={['#D7A02B', '#D7A02B']}
+          xtitle="Time of day"
+          ytitle="Average Price"
+          colors={["#D7A02B", "#D7A02B"]}
           library={{
-            animation: { duration: 1500, easing: 'linear' },
+            animation: { duration: 1500, easing: "linear" },
             scales: {
               yAxes: [
                 {
@@ -142,25 +164,42 @@ const SymbolChart = () => {
             },
           }}
         />
-        <div style={{ width: 800 }}>
+        <div style={{ width: 500 }}>
           <Autocomplete
-            id='stockInput'
+            id="stockInput"
             onInputChange={handleInput}
-            onChange={setProperInput}
             options={searchArray}
-            getOptionLabel={(stock) => stock.symbol + ' ' + stock.securityName}
+            onChange={setProperInput}
+            getOptionLabel={(stock) => stock.symbol + " " + stock.securityName}
             renderInput={(params) => (
               <TextField
                 {...params}
-                label='Select Company Ticker'
-                margin='normal'
-                variant='outlined'
+                label="Select Company Ticker"
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssFocused,
+                  },
+                }}
               />
             )}
+            InputProps={{
+              classes: {
+                root: classes.cssOutlinedInput,
+                focused: classes.cssFocused,
+                notchedOutline: classes.notchedOutline,
+              },
+            }}
           />
-          <Button variant='contained' color='primary'>
+          <GreenButton
+            variant="contained"
+            color="primary"
+            onClick={getSymbolInformation}
+          >
             Get Information
-          </Button>
+          </GreenButton>
         </div>
       </CardContent>
     </Card>
