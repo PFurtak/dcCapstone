a574a65b610cadb4853e95e3043ba5faa87c37ea
George M Bird <gmbirdiv@gmail.com>
2020-05-04 09:55:32 -0400

    changed latestPrice for daily gain to be only realtime as it SHOULD be 0 after day isover so daily gain is 0 as well

--- client/src/components/DailyGain.jsx
+++ client/src/components/DailyGain.jsx
@@ -18,19 +18,11 @@ const DailyGain = (props) => {
   };
 
   const getLatestPrice = async (symbol) => {
-    try {
-      let response = await fetch(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/quote/iexRealtimePrice?token=pk_135e66691d174c4291a33989af3f52c9`
-      );
-      let data = await response.json();
-      return data;
-    } catch {
-      let newResponse = await fetch(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=pk_135e66691d174c4291a33989af3f52c9`
-      );
-      let newData = await newResponse.json();
-      return newData;
-    }
+    let response = await fetch(
+      `https://cloud.iexapis.com/stable/stock/${symbol}/quote/iexRealtimePrice?token=pk_135e66691d174c4291a33989af3f52c9`
+    );
+    let data = await response.json();
+    return data;
   };
 
   useEffect(async function getPortfolioGains() {
