84b3a583552c8c039324207d9a60aa07fc51fa1c
George M Bird <gmbirdiv@gmail.com>
2020-05-04 09:41:51 -0400

    initial commit, changes to getLatestPrice with try catch block

--- client/src/components/PortfolioGain.jsx
+++ client/src/components/PortfolioGain.jsx
@@ -1,47 +1,83 @@
-import React, { useContext, useEffect, useState } from 'react';
-import FundContext from '../context/funds/fundContext';
+import React, { useContext, useEffect, useState } from "react";
+import FundContext from "../context/funds/fundContext";
 
+const PortfolioGains = (props) => {
+  const fundContext = useContext(FundContext);
+  const { funds } = fundContext;
+  const { fund } = props;
+  const [portfolioGains, setPortfolioGains] = useState(0);
 
-
-const PortfolioGains = props => {
-    const fundContext = useContext(FundContext);
-    const { funds } = fundContext;
-    const{fund }= props
-    const [portfolioGains, setPortfolioGains] = useState(0);
-
-  
-
-
-    const getLatestPrice = async(symbol) => {
-        let response = await fetch(`https://cloud.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=pk_135e66691d174c4291a33989af3f52c9`)
-        let data = await response.json()
-        return data          
-    }
-
-    useEffect(
-          async function getPortfolioGains() {
-            let oldvalue = 0; 
-            let portvalue = 0; 
-            let portgain = 0; 
-            for (const stock of fund.stocks){
-              oldvalue += stock.shares * stock.priceWhenAdded
-              portvalue += stock.shares * await getLatestPrice(stock.ticker)
-            }
-            portgain = ((portvalue - oldvalue) / oldvalue) * 100
-            setPortfolioGains(portgain)},
-        []
+  const getLatestPrice = async (symbol) => {
+    try {
+      let response = await fetch(
+        `https://cloud.iexapis.com/stable/stock/${symbol}/quote/iexRealtimePrice?token=pk_135e66691d174c4291a33989af3f52c9`
       );
-    
-    return (
-    
-    
-        <div>
-        <p> portfolio gain: {portfolioGains}%</p>
-        </div>
-     
-    )
- }
+      let data = await response.json();
+      return data;
+    } catch {
+      let newResponse = await fetch(
+        `https://cloud.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=pk_135e66691d174c4291a33989af3f52c9`
+      );
+      let newData = await newResponse.json();
+      return newData;
+    }
+  };
 
-export default PortfolioGains; 
+  useEffect(async function getPortfolioGains() {
+    let oldvalue = 0;
+    let portvalue = 0;
+    let portgain = 0;
+    for (const stock of fund.stocks) {
+      oldvalue += stock.shares * stock.priceWhenAdded;
+      portvalue += stock.shares * (await getLatestPrice(stock.ticker));
+    }
+    portgain = ((portvalue - oldvalue) / oldvalue) * 100;
+    setPortfolioGains(portgain);
+  }, []);
 
+  let gainStyle = {
+    color: "green",
+  };
 
+  let lossStyle = {
+    color: "red",
+  };
+  return (
+    <div>
+      {portfolioGains > 0 ? (
+        <p>
+          {" "}
+          portfolio gain:{" "}
+          <span style={gainStyle}>
+            {parseFloat(portfolioGains).toFixed(2)}%
+          </span>
+        </p>
+      ) : (
+        <p>
+          {" "}
+          portfolio gain:{" "}
+          <span style={lossStyle}>
+            {parseFloat(portfolioGains).toFixed(2)}%
+          </span>
+        </p>
+      )}
+      {portfolioGainsDollar > 0 ? (
+        <p>
+          Net portfolio gain:
+          <span style={gainStyle}>
+            ${parseFloat(portfolioGainsDollar).toFixed(2)}
+          </span>
+        </p>
+      ) : (
+        <p>
+          Net portfolio gain:
+          <span style={lossStyle}>
+            ${parseFloat(portfolioGainsDollar).toFixed(2)}
+          </span>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default PortfolioGains;
